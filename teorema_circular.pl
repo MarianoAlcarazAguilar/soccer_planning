47d69f6c06e6d5109ef920aaab1186b4f27d5aef
Mariano Alcaraz Aguilar <marianoaaguilar@hotmail.com>
2022-11-22 23:17:43 -0600

    Algunas nuevas funciones

--- teorema_circular.pl
+++ teorema_circular.pl
@@ -69,15 +69,73 @@ dame_partidos_jornada_individuales(Numero_jornada, Partido):-
 dame_partidos_jornada_individuales(_,_).
 
 
+/* saca_equipos_una_jornada(input, output)
+   donde:
+      input: el número de jornada que se busca
+      output: Los equipos que jugarán cada partido en la jornada en cuestión
+*/
+saca_equipos_una_jornada(Jornada, Equipos):-
+   dame_partidos_jornada_individuales(Jornada, Equipo_aux),
+   dame_equipos_partido(Equipo_aux, Equipos).
+
+/* encuentra_partido_local(input, output)
+   encuentra_partido_visitante(input, output) (basicamente funciona igual)
+   donde:
+      input: una lista con los equipos que disputarán el partido
+      output: el equipo local (básicamente es el primer equipo de la lista)
+*/
+encuentra_equipo_local([Equipo_local, _], Equipo_local):- !.
+
+encuentra_equipo_local(Partido, Equipo_local):-
+   dame_equipos_partido(Partido, Equipos),
+   encuentra_equipo_local(Equipos, Equipo_local).
+
+encuentra_equipo_visitante([_, Equipo_visitante], Equipo_visitante):- !.
+
+encuentra_equipo_visitante(Partido, Equipo_visitante):-
+   dame_equipos_partido(Partido, Equipos),
+   encuentra_equipo_visitante(Equipos, Equipo_visitante).
+
+
+/*
+Dado un número de jornada y un equipo local, encuentra el equipo visitante
+dame_contrincante(input_1, input_2, output)
+donde:
+   input_1: nombre del equipo local
+   input_2: numero de jornada que se busca
+   output: el nombre del equipo visitante contra el que juega el equipo local en la jornada dada
+*/
+dame_contrincante(Equipo_dado, Num_jornada, Equipo_contrincante):-
+   dame_partidos_jornada_individuales(Num_jornada, Partido_aux),
+   encuentra_equipo_local(Partido_aux, Local_aux),
+   encuentra_equipo_visitante(Partido_aux, Visitante_aux),
+   (
+      Equipo_dado == Local_aux -> 
+         encuentra_equipo_visitante(Partido_aux, Equipo_contrincante),
+         !;
+      (
+         Equipo_dado == Visitante_aux ->
+            encuentra_equipo_local(Partido_aux, Equipo_contrincante),
+            !;
+            true
+      )
+   ).
+
+
 /* El partido que ocasiona el movimiento es: New Castle vs Chelsea */
 
 /* dame_partidos_mover(input_1, input_2, input_3, output)
    donde:
-      input_1: el partido de la primera jornada que ocasiona el movimiento
+      input_1: el partido de la primera jornada que ocasiona el movimiento; puede ser un partido [ equipo, equipo ] o partido de la forma completa
       input_2: la jornada a
       input_3: la jornada b
       output: los partidos que se deben mover en ambas jornadas para asegurarnos de que los equipos se mantienen iguales 
       NOTA: posteriormente esto se va a cambiar a dos outputs, los partidos separados de cada jornada que hay que cambiar
 */
-%dame_partidos_mover(Partido, Jornada_1, Jornada_2, Partidos_mover):-
+auxiliar(Partido, _, _, _):-
+   /* Equipo_local es el equipo con el que se pretende terminar el ciclo */
+   encuentra_equipo_local(Partido, Equipo_local),
+   encuentra_equipo_visitante(Partido, Equipo_visitante).
+   
+
 
