0bd729813f0c3622ddaf8c6ef2b3497a70a049e0
Mariano Alcaraz Aguilar <marianoaaguilar@hotmail.com>
2022-11-27 23:02:09 -0600

    agrego paso 2: genera poblacion inicial

--- calendario_partidos_ai.pl
+++ calendario_partidos_ai.pl
@@ -10,6 +10,34 @@
 :- dynamic combinacion_equipos/1.
 :- dynamic combinacion_usada_jornada/2.
 
+
+/*************************************************
+Estas funciones se tienen que eliminar
+*************************************************/
+saca_equipos_una_jornada(Jornada, Equipos):-
+   dame_partidos_jornada(Jornada, Partidos),
+   recorre_partidos(Partidos, Equipos).
+
+
+recorre_partidos([], []):- !.
+recorre_partidos([[Equipos, _, _, _] | Resto_partidos], [Equipos | Resto_equipos]):-
+   recorre_partidos(Resto_partidos, Resto_equipos).
+
+dame_partidos_jornada(Numero_jornada, Partidos):-
+   findall(Partido, dame_partidos_jornada_aux(Numero_jornada, Partido), Partidos).
+
+dame_partidos_jornada_aux(Numero_jornada, Partido):-
+   partido(Partido),
+   dame_num_jornada_partido(Partido, Numero_jornada_aux),
+   Numero_jornada_aux == Numero_jornada.
+
+dame_num_jornada_partido([_, Num_jornada, _, _], Num_jornada).
+
+/*************************************************
+Eliminar hasta aquí
+*************************************************/
+
+
 /*************************
 PASO 1: Meter todos los datos de los equipos
 Almacena: nombre de equipo, posición temporada pasada, seguidores, ciudad, estadio
@@ -54,4 +82,195 @@ derby([chelsea, liverpool]).
 derby([brighton, crystal_palace]).
 derby([arsenal, man_united]).
 derby([chelsea, man_united]).
+
+
+/************************
+PASO 2: Generar vuelta inicial
+************************/
+
+dias([viernes, sabado, domingo]).
+horarios([14, 15, 16, 17, 18, 19, 20, 21, 22]).
+
+/* Regresa una lista con los nombres de todos los equipos */
+dame_nombres_equipos(Equipos):-
+   findall(X, equipo([X, _, _, _, _]), Equipos).
+
+/* Quiero generar todas las combinaciones de partidos entre los equipos */
+genera_contrincantes([]):- !.
+genera_contrincantes([Equipo | Resto]):-
+   genera_contrincantes(Resto),
+   combina_equipos(Equipo, Resto).
+
+combina_equipos(_, []):- !.
+combina_equipos(Equipo_uno, [Otro_equipo | Resto_equipos]):-
+   combina_equipos(Equipo_uno, Resto_equipos),
+   asserta(combinacion_equipos([Equipo_uno, Otro_equipo])).
+
+dame_lista_combinaciones_equipos(Combinaciones):-
+   findall(X, combinacion_equipos(X), Combinaciones).
+
+/* Esta función crea todas las combinaciones. Solo correr una vez. */
+genera_combinaciones:-
+   dame_nombres_equipos(Eq),
+   genera_contrincantes(Eq).
+
+elimina_combinaciones:-
+   retractall(combinacion_equipos(_)).
+
+elimina_partidos:-
+   retractall(partido(_)).
+
+elimina_combinacion_usada_jornada:-
+   retractall(combinacion_usada_jornada(_)).
+
+/* Esta función regresa los nombres de los equipos que ya están en una jornada, para asegurarnos que un nuevo partido sí puede entrar */
+dame_nombres_equipos_jornada(Num_jornada, Equipos):-
+   saca_equipos_una_jornada(Num_jornada, Aux_equipos),
+   flatten(Aux_equipos, Equipos).
+
+/* Funcion para elegir un dia de forma aleatoria */
+elige_dia_aleatorio(Dia):-
+   dias(Dias),
+   random_member(Dia, Dias).
+
+/* Funcion para elegir un horario de forma aleatoria */
+elige_hora_aleatorio(Hora):-
+   horarios(Horarios),
+   random_member(Hora, Horarios).
+
+/* Funcion para elegir una combinación aleatoria de equipos */
+elige_combinacion_aleatoria(Combinacion_aleatoria):-
+   dame_lista_combinaciones_equipos(X),
+   random_member(Combinacion_aleatoria, X).
+
+/* Función que regresa los nombres de los equipos dada una combinación */
+dame_equipos_de_combinacion([Equipo_1, Equipo_2], Equipo_1, Equipo_2).
+
+limpia:-
+   elimina_partidos,
+   elimina_combinaciones,
+   elimina_combinacion_usada_jornada,
+   genera_combinaciones.
+
+/* Si es 11, sabemos que ya metimos 10 partidos, entonces ya acabamos esa jornada */
+agrega_partidos_a_jornada(_, 11):- !.
+agrega_partidos_a_jornada(Num_jornada, Num_partido):-
+   dame_nombres_equipos_jornada(Num_jornada, Equipos_ya_participantes),
+   /* Primero elegimos un partido de forma aleatoria */
+   elige_combinacion_aleatoria(Combinacion_aleatoria),
+   dame_equipos_de_combinacion(Combinacion_aleatoria, Equipo_1, Equipo_2),
+   /* Segundo, vemos si esos equipos ya están jugando en la jornada especificada */
+   (not(member(Equipo_1, Equipos_ya_participantes)),
+    not(member(Equipo_2, Equipos_ya_participantes)) ->
+      /* Si ninguno de los dos equipos está en la jornada, entonces sí los metemos */
+      elige_dia_aleatorio(Dia),
+      elige_hora_aleatorio(Hora),
+      assertz(partido([[Equipo_1, Equipo_2], Num_jornada, Dia, Hora])),
+      /* Eliminamos la combinación de las combinaciones disponibles */
+      retract(combinacion_equipos([Equipo_1, Equipo_2])),
+      /* Llamamos a la función otra vez */
+      Otro_otro_num is Num_partido + 1,
+      agrega_partidos_a_jornada(Num_jornada, Otro_otro_num),
+      !
+      ;
+         /* Si alguno de los dos equipos sí esta en la jornada ya, no los podemos meter, 
+         entonces volvemos a llamar a la función sin aumentar el número de partidos */
+         agrega_partidos_a_jornada(Num_jornada, Num_partido)
+   ).
+
+/* Vamos a optimizar esta madre */
+agrega_optimizado(_, 11, _, _):- !.
+agrega_optimizado(Num_jornada, Num_partido, Num_iteracion, Num_desatoracion):-
+   (Num_desatoracion == 50 -> fail; true),
+   /* Vemos qué equipos YA ESTÁN jugando en la jornada: Equipos_ya_participantes */
+   dame_nombres_equipos_jornada(Num_jornada, Equipos_ya_participantes),
+   /* Vemos qué equipos FALTAN en la jornada: Equipos_faltantes */
+   dame_nombres_equipos(All_teams),
+   subtract(All_teams, Equipos_ya_participantes, Equipos_faltantes),
+   /* Elegimos DOS de los equipos faltantes: Equipo_1 y Equipo_2 */
+   random_member(Equipo_1, Equipos_faltantes),
+   delete(Equipos_faltantes, Equipo_1, Equipos_faltantes_2),
+   random_member(Equipo_2, Equipos_faltantes_2),
+   length(Equipos_faltantes, Size),
+   /* Ahora tenemos que ver si esa combinación se puede meter todavía;
+   Si existe la combinación, meto el partido, sino no */
+   (existe_combinacion_equipos(Equipo_1, Equipo_2) ->
+      elige_dia_aleatorio(Dia),
+      elige_hora_aleatorio(Hora),
+      assertz(partido([[Equipo_1, Equipo_2], Num_jornada, Dia, Hora])),
+      assertz(combinacion_usada_jornada([[Equipo_1, Equipo_2], Num_jornada])),
+      elimina_combinacion(Equipo_1, Equipo_2),
+      Otro_num is Num_partido + 1,
+      Aux_iter_1 is Num_iteracion + 1,
+      agrega_optimizado(Num_jornada, Otro_num, Aux_iter_1, Num_desatoracion),
+      !
+      ;  
+         (Size == 2 ->
+            /* En este punto ya estamos atorados, por lo que es necesario reinciar la jornada dada */
+            reinicia_combinaciones_usadas(Num_jornada),
+            Aux_iter_2 is Num_iteracion + 1,
+            agrega_optimizado(Num_jornada, 1, Aux_iter_2, Num_desatoracion)
+            ;
+               (Num_iteracion == 1000 ->
+                  reinicia_combinaciones_usadas(Num_jornada),
+                  Aux_desatoracion is Num_desatoracion + 1,
+                  agrega_optimizado(Num_jornada, 1, 1, Aux_desatoracion)
+                  ;
+                     Aux_iter_3 is Num_iteracion + 1,
+                     agrega_optimizado(Num_jornada, Num_partido, Aux_iter_3, Num_desatoracion)
+               )
+         )
+   ).
+
+/* Esta función te regresa true si existe una combinación disponible de los equipos dados */
+existe_combinacion_equipos(Equipo_1, Equipo_2):-
+   combinacion_equipos([Equipo_1, Equipo_2]),
+   !.
+
+existe_combinacion_equipos(Equipo_1, Equipo_2):-
+   combinacion_equipos([Equipo_2, Equipo_1]),
+   !.
+
+/* Esta función elimina una combinación de dos equipos dados si esta existe */
+elimina_combinacion(Equipo_1, Equipo_2):-
+   existe_combinacion_equipos(Equipo_1, Equipo_2),
+   combinacion_equipos([Equipo_1, Equipo_2]),
+   retract(combinacion_equipos([Equipo_1, Equipo_2])),
+   !.
+elimina_combinacion(Equipo_1, Equipo_2):-
+   existe_combinacion_equipos(Equipo_1, Equipo_2),
+   combinacion_equipos([Equipo_2, Equipo_1]),
+   retract(combinacion_equipos([Equipo_2, Equipo_1])),
+   !.
+
+/*
+Necesito una función que reinicie los partidos usados en una jornada dada
+*/
+dame_combinaciones_usadas_en_jornada(Num_jornada, Usadas):-
+   findall(X, combinacion_usada_jornada([X, Num_jornada]), Usadas).
+
+reinicia_combinaciones_usadas(_, []):- !.
+reinicia_combinaciones_usadas(Num_jornada, [Combinacion_usada | Resto_combinaciones]):-
+   reinicia_combinaciones_usadas(Num_jornada, Resto_combinaciones),
+   assertz(combinacion_equipos(Combinacion_usada)),
+   retract(combinacion_usada_jornada([Combinacion_usada, Num_jornada])),
+   retract(partido([Combinacion_usada, Num_jornada, _, _])).
+
+reinicia_combinaciones_usadas(Num_jornada):-
+   dame_combinaciones_usadas_en_jornada(Num_jornada, Usadas),
+   reinicia_combinaciones_usadas(Num_jornada, Usadas).
+
+/* Métod para generar una vuelta aleatorioa */
+genera_vuelta:-
+   limpia,
+   genera_vuelta(1),
+   write('Vuelta Terminada'),nl.
+
+genera_vuelta(20):- !.
+genera_vuelta(Num_jornada):-
+   agrega_optimizado(Num_jornada, 1, 1, 0),
+   write(Num_jornada),nl,
+   Sig_jornada is Num_jornada + 1,
+   (genera_vuelta(Sig_jornada) -> true; write('Repitiendo Vuelta'),nl,genera_vuelta, !).
+
 
