2ece6b34f4d88abfba023638b4385fe48fcf0ab9
Mariano Alcaraz Aguilar <marianoaaguilar@hotmail.com>
2022-11-29 14:35:02 -0600

    Commit automático de script ejecuta_programa.sh

--- ejecuta_programa.sh
+++ ejecuta_programa.sh
@@ -7,3 +7,9 @@ python ./python_files/grafica_progreso.py
 
 echo 'Eliminando cosas innecesarias (a.out)'
 rm a.out
+
+echo 'Guardando Cambios en Git'
+sleep 1
+git add .
+sleep 1
+git commit -m 'Commit automático de script ejecuta_programa.sh'
