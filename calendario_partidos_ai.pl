b1399332660a374073ce5e1e22f81f8021849829
Mariano Alcaraz Aguilar <marianoaaguilar@hotmail.com>
2022-11-29 11:31:28 -0600

    h

--- calendario_partidos_ai.pl
+++ calendario_partidos_ai.pl
@@ -917,7 +917,7 @@ mainmut:-
    dame_jornadas_en_lista(NuevaLista),
    rating_vuelta(NuevaLista, Rating),
    write(Rating),nl,
-   (  Rating < 0.83
+   (  Rating < 0.8
    -> maincruz
    ;  fin(NuevaLista, Rating), !
    ).
@@ -930,7 +930,7 @@ maincruz:-
    dame_jornadas_en_lista(NuevaLista),
    rating_vuelta(NuevaLista, Rating),
    write(Rating),nl,
-   (  Rating < 0.83
+   (  Rating < 0.8
    -> mainmut
    ;  fin(NuevaLista, Rating), !
    ).
